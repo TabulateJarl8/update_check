4ee6f4f75af42d9da280e65f6d6ca4da63f2165a
Tabulate <58576759+TabulateJarl8@users.noreply.github.com>
2020-04-20 00:23:45 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="update-check",
-    version="0.0.1",
+    name="update_check",
+    version="0.0.4",
     author="Tabulate",
     author_email="tabulatejarl8@gmail.com",
     description="Package to update the end-users copy of your file to the latest version",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TabulateJarl8/update-check",
+    url="https://github.com/TabulateJarl8/update_check",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
